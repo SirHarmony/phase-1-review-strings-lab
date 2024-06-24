b7f3cef1ba7616a88526ea1a67ff50534e3dc8e2
Harmony <harmonymukolwe@gmail.com>
2024-06-25 01:07:36 +0300

    Reviews strings

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+const currentUser = "Zeki Odhoge";
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = `${welcomeMessage.toUpperCase()} ${currentUser}!`;
+
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
